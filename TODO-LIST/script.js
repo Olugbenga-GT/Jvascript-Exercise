5e9ce0a07623845a77997e8965516fbbcae65669
Olugbenga Alao <gasttronomy@gmail.com>
2021-03-17 21:37:23 +0100

    Now, it is fully functional. you can remove and clear list from UI and Database upon reload

--- TODO-LIST/script.js
+++ TODO-LIST/script.js
@@ -14,6 +14,7 @@ function loadEventListeners(){
  taskList.addEventListener('click', removeTask);
  clearBtn.addEventListener('click', clearTask);
  filter.addEventListener('keyup', filterTasks);
+ document.addEventListener('DOMContentLoaded', getTasksFromLocalStorage);
  
 }
 
@@ -27,27 +28,95 @@ function loadEventListeners(){
             const link = document.createElement('a');
             link.className = 'delete-item secondary-content';
             link.innerHTML = '<i class =" fa fa-remove"></i>' // adds the font awesome remove icon to the a tag
+
             li.appendChild(link); //appends link to li
             taskList.appendChild(li); //appends li to UL with class task list 
+
+            // store task in Local Storage
+            storeTaskInLocalStorage(taskInput.value);
             taskInput.value = '';
 
       e.preventDefault();
       }
 
+      function storeTaskInLocalStorage(task){
+            let tasks;
+            if(localStorage.getItem('tasks') === null){
+                  tasks = [ ];
+            }
+            else {
+                  tasks = JSON.parse(localStorage.getItem('tasks'));
+            }
+                  tasks.push(task);
+
+                  localStorage.setItem('tasks',   JSON.stringify(tasks));
+      }
+
+      function getTasksFromLocalStorage(){
+            let tasks;
+            if(localStorage.getItem('tasks') === null){
+                  tasks = [ ];
+            }
+            else {
+                  tasks = JSON.parse(localStorage.getItem('tasks'));
+            }
+
+            tasks.forEach(function(task){
+                  const li =  document.createElement('li');
+                  li.className = 'collection-item'; // This creates the li
+                  li.appendChild(document.createTextNode(task)); // Adds the input value to the li
+                  const link = document.createElement('a');
+                  link.className = 'delete-item secondary-content';
+                  link.innerHTML = '<i class =" fa fa-remove"></i>' // adds the font awesome remove icon to the a tag
+      
+                  li.appendChild(link); //appends link to li
+                  taskList.appendChild(li); //appends li to UL with class task list 
+            })
+      }
 
       function removeTask(e){
             if(e.target.parentElement.classList.contains('delete-item')){
                   if(confirm("Are you sure ?")){
                         e.target.parentElement.parentElement.remove();
+
+                        // Remove from LS
+                        removeTaskFromLocalStorage(e.target.parentElement.parentElement);
                   }
             }
       }
+
+      function removeTaskFromLocalStorage(taskItem){
+            let tasks;
+            if(localStorage.getItem('tasks') === null){
+                  tasks = [ ];
+            }
+            else {
+                  tasks = JSON.parse(localStorage.getItem('tasks'));
+            }
+
+            tasks.forEach(function(task, index){
+                  if(taskItem.textContent=== task){
+                        tasks.splice(index  ,  1);
+                  } 
+            });
+            localStorage.setItem('tasks', JSON.stringify(tasks))
+      }
+
+
+
 
       function clearTask(e){
             // taskList.innerHTML = ''; (This also works)
              while(taskList.firstChild){
                    taskList.removeChild(taskList.firstChild);
              }
+
+             clearTaskFromLocalStorage();
+      }
+
+
+      function clearTaskFromLocalStorage(){
+            localStorage.clear();
       }
 
    function filterTasks(e){
@@ -64,3 +133,6 @@ function loadEventListeners(){
          })
 
    }
+
+//    Persisting to Local Storage
+
