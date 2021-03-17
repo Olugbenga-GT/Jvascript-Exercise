1d5f21b4c1924830587a00f300f2a4bc0bfe2b15
Olugbenga Alao <gasttronomy@gmail.com>
2021-03-17 20:44:55 +0100

    Now, the to do can add, remove, clear and filter tasks

--- TODO-LIST/script.js
+++ TODO-LIST/script.js
@@ -11,6 +11,12 @@ loadEventListeners();
 function loadEventListeners(){
 
  form.addEventListener('submit', addTask);
+ taskList.addEventListener('click', removeTask);
+ clearBtn.addEventListener('click', clearTask);
+ filter.addEventListener('keyup', filterTasks);
+ 
+}
+
       function addTask(e){
             if(taskInput.value === ''){
                   alert("Please, Add a task")
@@ -28,4 +34,33 @@ function loadEventListeners(){
       e.preventDefault();
       }
 
-}
+
+      function removeTask(e){
+            if(e.target.parentElement.classList.contains('delete-item')){
+                  if(confirm("Are you sure ?")){
+                        e.target.parentElement.parentElement.remove();
+                  }
+            }
+      }
+
+      function clearTask(e){
+            // taskList.innerHTML = ''; (This also works)
+             while(taskList.firstChild){
+                   taskList.removeChild(taskList.firstChild);
+             }
+      }
+
+   function filterTasks(e){
+         const text = e.target.value.toLowerCase();
+         document.querySelectorAll('.collection-item').forEach(function(task){
+               const item = task.firstChild.textContent;
+               if(item.toLowerCase().indexOf(text)  != -1){
+                  task.style.display = 'block';
+               }
+               else { 
+                     task.style.display = 'none';
+               }
+
+         })
+
+   }
